ddba1bb8c3de37fd7f853752dc40033bacc9348f
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-07-09 20:49:56 +0400

    Feat: Implemented IsAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { IsAuthGuard } from './guard/isAuth.gurad';
+import { UserId } from 'src/users/decorator/user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +16,11 @@ export class AuthController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+  @Get('current-user')
+  @UseGuards(IsAuthGuard)
+  getCurrentuser(@UserId() userId) {
+    console.log(userId);
+    return this.authService.getCurrentUser(userId);
   }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,15 +6,18 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { IsAuthGuard } from 'src/auth/guard/isAuth.gurad';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards(IsAuthGuard)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
@@ -29,12 +32,12 @@ export class ProductsController {
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
-
+  @UseGuards(IsAuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
-
+  @UseGuards(IsAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
