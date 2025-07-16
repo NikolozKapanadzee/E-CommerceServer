36709787cc44911073e7dac33cbbe0c7951601b5
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-07-17 03:17:19 +0400

    Fix: (aws-s3.service.ts) get-file Function

--- src/aws-s3/aws-s3.service.ts
+++ src/aws-s3/aws-s3.service.ts
@@ -5,12 +5,12 @@ import {
   S3Client,
 } from '@aws-sdk/client-s3';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { Readable } from 'stream';
 
 @Injectable()
 export class AwsS3Service {
   private bucketName;
   private s3;
-
   constructor() {
     this.bucketName = process.env.AWS_BUCKET_NAME;
     this.s3 = new S3Client({
@@ -32,7 +32,6 @@ export class AwsS3Service {
       Bucket: this.bucketName,
       ContentType: file.mimetype,
     };
-
     const uploadCommand = new PutObjectCommand(config);
     await this.s3.send(uploadCommand);
     return {
@@ -42,16 +41,24 @@ export class AwsS3Service {
   }
 
   async getFileById(fileId) {
-    if (!fileId) throw new BadRequestException('fileId is required');
+    if (!fileId) throw new BadRequestException('fileId is requied');
     const config = {
       Key: fileId,
       Bucket: this.bucketName,
     };
     const getCommand = new GetObjectCommand(config);
     const fileStream = await this.s3.send(getCommand);
-    console.log(fileStream, 'filestrewam');
+    if (fileStream.Body instanceof Readable) {
+      const chunks: any = [];
+      for await (const chunk of fileStream.Body) {
+        chunks.push(chunk);
+      }
+      const fileBuffer = Buffer.concat(chunks);
+      const base64 = fileBuffer.toString('base64');
+      const file = `data:${fileStream.ContentType};base64,${base64}`;
+      return file;
+    }
   }
-
   async deleteFileById(fileId) {
     if (!fileId) throw new BadRequestException('fileId is required');
     const config = {
