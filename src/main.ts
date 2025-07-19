83eb970dd246e10f48fb1cd21ab684b5e24d571f
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-07-19 08:26:14 +0400

    Fix: Build Error

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('tiny'));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.use('/stripe/webhook', express.raw({ type: 'application/json' }));
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
