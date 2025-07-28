93895a8d4365c62ab4bba6be13f5f35fee62e068
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-07-28 19:28:41 +0400

    Fix: Cors Error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://e-commerce-client-flame.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
